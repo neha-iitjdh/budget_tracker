343bc0f096d9bfbf2851b595f32fd6d92285c370
Neha Sharma <neha.iitjdh@gmail.com>
2025-06-19 23:57:33 +0530

    New Files Added

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-require('dotenv').config(); // Load .env variables into process.ENV
-console.log(process.env.JWT_SECRET); // Log the PORT variable to verify it's loaded correctly
+const express = require('express');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+const redisClient = require('./services/redisService');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./docs/swaggerConfig');
+const { connectToKafka } = require('./apache-kafka/kafkaService');
+const routes = require('./routes');
+const config = require('./config/config');
+const { connectToRabbitMQ } = require('./services/rabbitMQService');
+const seedMongoData = require('./services/dataSeeder');
+const startGrpcServer = require('./grpcServer');
+const cors = require('cors');
+const morgan = require('morgan');
+const favicon = require('serve-favicon');
+
+// Environment Variable Configuration
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const app = express();
+
+// Favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// CORS
+app.use(cors()); // Enable All CORS Requests
+
+// Body Parser Middleware
+app.use(express.json());
+
+// Request Logging Middleware
+app.use(morgan('dev')); // Logs all incoming requests to the console
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,6 @@
+const { verifyToken } = require('../services/jwtService');
+const redisClient = require('../services/redisService');
+
+const authenticate = async (req, res, next) => {};
+
+module.exports = authenticate;
