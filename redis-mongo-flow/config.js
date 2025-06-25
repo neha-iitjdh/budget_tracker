62e11673263c9ed697123f4ea7fabf157731c222
Neha Sharma <neha.iitjdh@gmail.com>
2025-06-25 16:42:40 +0530

    redis-mongo-flow wrapped up

---
+++ redis-mongo-flow/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://nehaiitjdh:tutu1234@cluster0.vc1ufyl.mongodb.net/budget_manager?retryWrites=true&w=majority',
+  redisHost: '127.0.0.1',
+  redisPort: 6379,
+  rabbitMQHost: 'localhost',
+  port: 3001,
+};
