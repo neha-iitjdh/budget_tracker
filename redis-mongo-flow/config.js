78da81f6ec5f64b48ba31acf35fd5cef7a70fcc5
Neha Sharma <neha.iitjdh@gmail.com>
2025-06-25 18:11:49 +0530

    Security Issue Fixed

--- redis-mongo-flow/config.js
+++ redis-mongo-flow/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://nehaiitjdh:tutu1234@cluster0.vc1ufyl.mongodb.net/budget_manager?retryWrites=true&w=majority',
+  mongoURI: 'mongodb://localhost:27017/myDatabase',
   redisHost: '127.0.0.1',
   redisPort: 6379,
   rabbitMQHost: 'localhost',
