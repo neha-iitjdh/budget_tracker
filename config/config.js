4848f56a606452124983345b7847926bde3eea59
Neha Sharma <neha.iitjdh@gmail.com>
2025-07-20 16:47:18 +0530

    config folder with docker setup done

---
+++ config/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+// Load environment variables from .env file
+module.exports = {
+  mongoURI: process.env.MONGO_DB_URI,
+  redisUrl: process.env.REDIS_URL,
+  rabbitMQUrl: process.env.RABBITMQ_URL,
+  kafkaBroker: process.env.KAFKA_BROKER,
+  jwtSecret: process.env.JWT_SECRET,
+  elasticSearchUrl: process.env.ELASTIC_SEARCH_URL,
+  postgresUrl: process.env.POSTGRES_URL,
+};
